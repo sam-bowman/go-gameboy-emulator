11a8395433902ce0a95b21fcdcdead770083586e
Hoshiwara <onishiwatsu@gmail.com>
2025-02-24 15:14:55 +0000

    switch to result naming

--- emulator/CPU_opcodes_unprefixed.go
+++ emulator/CPU_opcodes_unprefixed.go
@@ -146,9 +146,9 @@ func ADD_0x09_HL_BC(GB *GAMEBOY) {
 	log.Println("0x09 ADD HL_BC")
 	combinedLeft := uint16(GB.CPU._r.H)<<8 | uint16(GB.CPU._r.L)
 	combinedRight := uint16(GB.CPU._r.B)<<8 | uint16(GB.CPU._r.C)
-	combinedLeft = combinedLeft + combinedRight
-	GB.CPU._r.H = byte(combinedLeft >> 8)
-	GB.CPU._r.L = byte(combinedLeft & 0xFF)
+	result := combinedLeft + combinedRight
+	GB.CPU._r.H = byte(result >> 8)
+	GB.CPU._r.L = byte(result & 0xFF)
 	//NEEDS CODE (FLAG HANDLING)
 	GB.CPU._r.F.N = 0
 
@@ -365,9 +365,9 @@ func ADD_0x19_HL_DE(GB *GAMEBOY) {
 	log.Println("0x19 ADD HL_DE")
 	combinedLeft := uint16(GB.CPU._r.H)<<8 | uint16(GB.CPU._r.L)
 	combinedRight := uint16(GB.CPU._r.D)<<8 | uint16(GB.CPU._r.E)
-	combinedLeft = combinedLeft + combinedRight
-	GB.CPU._r.H = byte(combinedLeft >> 8)
-	GB.CPU._r.L = byte(combinedLeft & 0xFF)
+	result := combinedLeft + combinedRight
+	GB.CPU._r.H = byte(result >> 8)
+	GB.CPU._r.L = byte(result & 0xFF)
 	//NEEDS CODE (FLAG HANDLING)
 	GB.CPU._r.F.N = 0
 
@@ -599,9 +599,9 @@ func ADD_0x29_HL_HL(GB *GAMEBOY) {
 	log.Println("0x29 ADD HL_HL")
 	combinedLeft := uint16(GB.CPU._r.H)<<8 | uint16(GB.CPU._r.L)
 	combinedRight := uint16(GB.CPU._r.H)<<8 | uint16(GB.CPU._r.L)
-	combinedLeft = combinedLeft + combinedRight
-	GB.CPU._r.H = byte(combinedLeft >> 8)
-	GB.CPU._r.L = byte(combinedLeft & 0xFF)
+	result := combinedLeft + combinedRight
+	GB.CPU._r.H = byte(result >> 8)
+	GB.CPU._r.L = byte(result & 0xFF)
 	//NEEDS CODE (FLAG HANDLING)
 	GB.CPU._r.F.N = 0
 
@@ -800,9 +800,9 @@ func ADD_0x39_HL_SP(GB *GAMEBOY) {
 	log.Println("0x39 ADD HL_SP")
 	combinedLeft := uint16(GB.CPU._r.H)<<8 | uint16(GB.CPU._r.L)
 	combinedRight := GB.CPU._r.SP
-	combinedLeft = combinedLeft + combinedRight
-	GB.CPU._r.H = byte(combinedLeft >> 8)
-	GB.CPU._r.L = byte(combinedLeft & 0xFF)
+	result := combinedLeft + combinedRight
+	GB.CPU._r.H = byte(result >> 8)
+	GB.CPU._r.L = byte(result & 0xFF)
 	//NEEDS CODE (FLAG HANDLING)
 	GB.CPU._r.F.N = 0
 
