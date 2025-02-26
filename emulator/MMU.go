7bc4f4f3aae0807df140a37c2b6702a199659473
Hoshiwara <onishiwatsu@gmail.com>
2025-02-26 13:36:40 +0000

    whitespace

--- emulator/MMU.go
+++ emulator/MMU.go
@@ -12,30 +12,41 @@ func readByte(GB *GAMEBOY, addr uint16) uint8 {
 			}
 			return GB.MMU._rom[addr]
 		}
+
 	case 0x1000, 0x2000, 0x3000: //ROM0
 		return GB.MMU._rom[addr]
+
 	case 0x4000, 0x5000, 0x6000, 0x7000: //ROM1
 		return GB.MMU._rom[addr]
+
 	case 0x8000, 0x9000: //Graphics-VRAM
 		return GB.GPU._vram[addr&0x1FFF]
+
 	case 0xA000, 0xB000: //ERAM
 		return GB.MMU._eram[addr&0x1FFF]
+
 	case 0xC000, 0xD000: //WRAM
 		return GB.MMU._wram[addr&0x1FFF]
+
 	case 0xE000: //WRAM-Shadow
 		return GB.MMU._wram[addr&0x1FFF]
+
 	case 0xF000: //WRAM-Shadow, I/O, Zero-Page RAM
 		switch addr & 0x0F00 {
+
 		case 0x000, 0x100, 0x200, 0x300, 0x400, 0x500, 0x600: //WRAM-Shadow
 			return GB.MMU._wram[addr&0x1FFF]
+
 		case 0x700, 0x800, 0x900, 0xA00, 0xB00, 0xC00, 0xD00: //WRAM-Shadow
 			return GB.MMU._wram[addr&0x1FFF]
+
 		case 0xE00: //Graphics-OAM
 			if addr < 0xFAE0 {
 				return GB.GPU._oam[addr&0xFF]
 			} else {
 				return 0
 			}
+
 		case 0xF00: //Zero-Page
 			if addr >= 0xFF80 {
 				return GB.MMU._zram[addr&0x7F]
@@ -46,6 +57,7 @@ func readByte(GB *GAMEBOY, addr uint16) uint8 {
 			}
 		}
 		return GB.MMU._wram[addr&0x1FFF]
+
 	}
 
 	return 0
